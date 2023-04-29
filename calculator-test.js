bbcbd972f0480da5323f0100022827af35e6fcfe
John Bonilla <bonilla402@gmail.com>
2023-04-29 13:07:36 -0400

    Exercise Finalized

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,33 @@
 
-it('should calculate the monthly rate correctly', function () {
+it('should calculate the monthly rate correctly', function() {
   // ...
+  const values = {
+    amount: 1500.5,
+    years: 5.5,
+    rate: 3.7
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('25.16');
+});
+
+it('should calculate the monthly rate correctly', function() {
+  // ...
+  const values = {
+    amount: 200000,
+    years: 10,
+    rate: 10
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('2643.01');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: Math.random()*1000,
+    years: Math.random()*10,
+    rate: Math.random()*10
+  };
+  expect(CountDecimalPlaces(calculateMonthlyPayment(values))).toEqual(2);
+  
 });
 
 /// etc
--- calculator.js
+++ calculator.js
@@ -21,20 +21,53 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const amountInput =  document.getElementById("loan-amount");
+  const yearsInput =  document.getElementById("loan-years");
+  const rateInput =  document.getElementById("loan-rate");
+
+  amountInput.value = 1000;
+  yearsInput.value = 8;
+  rateInput.value = 5.8;
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let currentValues = getCurrentUIValues();
+  let montlyPayment = calculateMonthlyPayment(currentValues);
+  updateMonthly(montlyPayment);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let i = (values.rate/(100)/12);
+  let p =  values.amount;
+  let n = values.years * 12 * -1;
+
+
+  let monthlyAmt =  p * i;
+  let loanSpread = 1 - Math.pow(( 1 + i), n);
+
+  if(loanSpread !== 0){
+    monthlyAmt = monthlyAmt/loanSpread;
+  }
+  else{
+    monthlyAmt = 0;
+  }
+    return monthlyAmt.toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlySpan =  document.getElementById("monthly-payment");
+  monthlySpan.innerText = monthly; 
+}
+
+
+function CountDecimalPlaces(stringValue){
+  return stringValue.length - stringValue.indexOf(".")-1;
 }
